2f125e9ae37ce38b084641b6feaf36e2a836b1c3
Tibi2003 <baolam24112003@gmail.com>
2023-09-25 17:57:35 +0700

    hoan thanh bai 2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Bài 1
 function ketQuaDuThi() {
     var diem1 = document.getElementById("diem-mon-1").value * 1;
     var diem2 = document.getElementById("diem-mon-2").value * 1;
@@ -44,4 +45,44 @@ function ketQuaDuThi() {
       tongDiem,
     });
   }
+
+// Bài 2
+function tinhTienDien() {
+    var soDien = document.getElementById("so-dien").value * 1;
+    var hoTen = document.getElementById("ho-ten").value;
+    var tienDien50kwDau = 500;
+    var tienDien50Den100kw = 650;
+    var tienDien100Den200kw = 850;
+    var tienDien200Den350kw = 1100;
+    var tienDienCaoNhat = 1300;
+    var tienDien;
+    if (soDien <= 50) {
+      tienDien = soDien * tienDien50kwDau;
+    } else if (soDien > 50 && soDien <= 100) {
+      tienDien = 50 * tienDien50kwDau + (soDien - 50) * tienDien50Den100kw;
+    } else if (soDien > 100 && soDien <= 200) {
+      tienDien =
+        50 * tienDien50kwDau +
+        50 * tienDien50Den100kw +
+        (soDien - 100) * tienDien100Den200kw;
+    } else if (soDien > 200 && soDien <= 350) {
+      tienDien =
+        50 * tienDien50kwDau +
+        50 * tienDien50Den100kw +
+        100 * tienDien100Den200kw +
+        (soDien - 200) * tienDien200Den350kw;
+    } else {
+      tienDien =
+        50 * tienDien50kwDau +
+        50 * tienDien50Den100kw +
+        100 * tienDien100Den200kw +
+        150 * tienDien200Den350kw +
+        (soDien - 350) * tienDienCaoNhat;
+    }
+  
+    document.getElementById(
+      "sotiendien"
+    ).innerText = `Họ tên: ${hoTen}, số tiền điện bạn phải trả là ${tienDien.toLocaleString()} VND`;
+  }
+  
   
